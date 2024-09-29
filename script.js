94d4ffe4d9107fba9d4b5e7f1519037eb2af5efb
Raphael Salamon <137216688+RaphSalamon@users.noreply.github.com>
2024-09-29 04:11:08 -0400

    75% done 9/29/24 at 4:10 AM

--- script.js
+++ script.js
@@ -44,30 +44,30 @@ const locations = [
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\"."
+    text: "CHOOSE YOUR NEXT MOVE. \"Store\"."
   },
   {
     name: "store",
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goTown],
+    "button functions": [buyHealth, buyWeapon, MAIN],
     text: "You enter the store."
   },
   {
     name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
+    "button functions": [fightSlime, fightBeast, MAIN],
     text: "You enter the cave. You see some monsters."
   },
   {
     name: "fight",
     "button text": ["Attack", "Dodge", "Run"],
-    "button functions": [attack, dodge, goTown],
+    "button functions": [attack, dodge, MAIN],
     text: "You are fighting a monster."
   },
   {
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
-    "button functions": [goTown, goTown, easterEgg],
+    "button functions": [MAIN, MAIN, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   },
   {
@@ -85,7 +85,7 @@ const locations = [
   {
     name: "easter egg",
     "button text": ["2", "8", "Go to town square?"],
-    "button functions": [pickTwo, pickEight, goTown],
+    "button functions": [pickTwo, pickEight, MAIN],
     text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
   }
 ];
@@ -106,7 +106,7 @@ function update(location) {
   text.innerHTML = location.text;
 }
 
-function goTown() {
+function MAIN() {
   update(locations[0]);
 }
 
@@ -249,7 +249,7 @@ function restart() {
   goldText.innerText = gold;
   healthText.innerText = health;
   xpText.innerText = xp;
-  goTown();
+  MAIN();
 }
 
 function easterEgg() {
